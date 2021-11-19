690913f7543487c80991b7478bc5eaeaa14653ff
Haoran Teng <hteng@mcw.edu>
2021-11-18 21:17:23 -0600

    Update DECRIPTION

---
+++ regexcite_develop.R
@@ -0,0 +1,7 @@
+library(devtools)
+use_git()
+use_github()
+library(desc)
+desc_add_author(given="Haoran", family="Teng", email="hteng@mcw.edu",
+                role=c("aut","cre"))
+use_mit_license()
